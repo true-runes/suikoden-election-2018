c41b135a6e182c5d6a0db9852cab0fc0645e837a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-05-20 19:43:53 +0900

    [update] db create first step

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_082552) do
+ActiveRecord::Schema.define(version: 2018_05_20_102443) do
 
-  create_table "tweets", force: :cascade do |t|
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "tweet_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
