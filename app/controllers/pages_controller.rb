c3321884531f4c9711d0b4417912a9fef309c33f
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-22 21:00:27 +0900

    [modify] JST (NOT COOL, MUST BE MODIFIED)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @vote_number = Tweet.where(is_retweet: 0).where(tweeted_at: '2018-06-22 21:00:00'..'2018-06-24 09:00:00').size
+    @vote_number = Tweet.where(is_retweet: 0).where(tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo')).size
   end
 end
