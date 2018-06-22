3f2ac29f7922cb3307bd44dfcf0da4c8163101e4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-22 20:51:41 +0900

    [update] add refresh meta tag for fake realtime update... (NOT COOL so will be modified)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,5 @@
 class PagesController < ApplicationController
+  def home
+    @vote_number = Tweet.where(is_retweet: 0).where(tweeted_at: '2018-06-22 21:00:00'..'2018-06-24 09:00:00').size
+  end
 end
