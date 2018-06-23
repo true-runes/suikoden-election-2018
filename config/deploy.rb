687f5f9edd78627ec0bdcf959a52806b9868cb6d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 11:14:12 +0900

    [update] update yarn directory at local more

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ndenv_type, :user
 set :ndenv_node, '10.5.0'
-set :yarn_target_path, -> { "/home/deploy_gss_2018_production/.ndenv/shims/yarn" }
+set :yarn_target_path, -> { "/home/deploy_gss_2018_production/.ndenv/shims" }
 
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
