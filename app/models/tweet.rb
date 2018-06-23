42ced81a7cc7dcef9d9b3012b4c03fe71d365afb
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-24 00:18:12 +0900

    [update] valid votes' tweet_numbers

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -12,4 +12,8 @@ class Tweet < ApplicationRecord
   def valid_vote_tweets
     Tweet.where(is_retweet: 0).where(tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo'))
   end
+
+  def tweet_numbers_of_valid_vote_tweets
+    valid_vote_tweets.map { |tweet| tweet.tweet_number }
+  end
 end
