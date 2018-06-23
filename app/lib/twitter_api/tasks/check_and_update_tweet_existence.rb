2fefd7b1e60f39ff2d7b5f286f9a919dc31a24c7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-24 08:24:36 +0900

    [fix] fix id is str to integer more^2

--- app/lib/twitter_api/tasks/check_and_update_tweet_existence.rb
+++ app/lib/twitter_api/tasks/check_and_update_tweet_existence.rb
@@ -24,17 +24,19 @@ class TwitterApi::Tasks::CheckAndUpdateTweetExistence
 
   def self.debug_execute
     all_tweet_numbers = Tweet.new.tweet_numbers_of_valid_vote_tweets
-    divided_tweet_numbers = all_tweet_numbers.each_slice(100).to_a
+    divided_tweet_numbers = all_tweet_numbers.each_slice(100).to_a # 要素は Integer
 
     divided_tweet_numbers.each do |tweet_numbers|
       alive_tweet_objects = twitter_api_client.statuses(tweet_numbers)
 
-      alive_tweet_numbers = alive_tweet_objects.map { |alive_tweet_object| alive_tweet_object.id.to_i }
+      alive_tweet_numbers = alive_tweet_objects.map { |alive_tweet_object| alive_tweet_object.id }
       not_alive_tweet_numbers = tweet_numbers - alive_tweet_numbers
 
       # soft delete by paranoia
       not_alive_tweet_records = Tweet.where(tweet_number: not_alive_tweet_numbers)
     end
+
+    not_alive_tweet_records
   end
 
   def self.execute_with_argument(all_tweet_numbers)
