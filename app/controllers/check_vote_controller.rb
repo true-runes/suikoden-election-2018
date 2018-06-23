00db899dc1b387f687f5d8c8b8250ba1d216a34d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 17:19:40 +0900

    [update] update frontend

--- app/controllers/check_vote_controller.rb
+++ app/controllers/check_vote_controller.rb
@@ -3,11 +3,17 @@ class CheckVoteController < ApplicationController
     if params[:screen_name]
       @screen_name = params[:screen_name]
 
-      if User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0, tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo') }).first.nil?
+      if User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0 }).first.nil?
         @target_tweets = []
       else
-        @target_tweets = User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0, tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo') }).first.tweets
+        @target_tweets = User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0 }).first.tweets
       end
+
+      # if User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0, tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo') }).first.nil?
+      #   @target_tweets = []
+      # else
+      #   @target_tweets = User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0, tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo') }).first.tweets
+      # end
 
       @vote_number = @target_tweets.size
 
@@ -15,6 +21,7 @@ class CheckVoteController < ApplicationController
         @user_name = nil
       else
         @user_name = User.where(screen_name: @screen_name).first.name
+        @profile_image_uri = User.where(screen_name: @screen_name).first.profile_image_uri
       end
     end
   end
