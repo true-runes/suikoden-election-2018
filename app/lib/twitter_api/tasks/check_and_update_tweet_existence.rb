b6ee03960a3275f0e200c029cb417adf5f4d9656
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-24 07:07:47 +0900

    [update] complete to create deleted tweet check method

--- app/lib/twitter_api/tasks/check_and_update_tweet_existence.rb
+++ app/lib/twitter_api/tasks/check_and_update_tweet_existence.rb
@@ -5,26 +5,6 @@ class TwitterApi::Tasks::CheckAndUpdateTweetExistence
     all_tweet_numbers = Tweet.new.tweet_numbers_of_valid_vote_tweets
     divided_tweet_numbers = all_tweet_numbers.each_slice(100).to_a
 
-    # HACK: NOT DRY...?
-    divided_tweet_numbers.each do |tweet_numbers|
-      tweet_objects = twitter_api_client.statuses(tweet_numbers)
-      user_objects = TwitterApi::ExchangeTweetToUser.execute(tweet_objects);
-
-      # 下の2つの順番には依存性がある
-      TwitterApi::UpsertObjects::UsersTable.upsert(user_objects)
-      TwitterApi::UpsertObjects::TweetsTable.upsert(tweet_objects)
-
-      TwitterApi::UpsertObjects::InUserUrisTable.upsert(user_objects)
-      TwitterApi::UpsertObjects::HashtagsTable.upsert(tweet_objects)
-      TwitterApi::UpsertObjects::InTweetUrisTable.upsert(tweet_objects)
-      TwitterApi::UpsertObjects::MediaTable.upsert(tweet_objects)
-    end
-  end
-
-  def self.debug_execute
-    all_tweet_numbers = Tweet.new.tweet_numbers_of_valid_vote_tweets
-    divided_tweet_numbers = all_tweet_numbers.each_slice(100).to_a
-
     divided_tweet_numbers.each do |tweet_numbers|
       alive_tweet_objects = twitter_api_client.statuses(tweet_numbers)
 
@@ -36,11 +16,12 @@ class TwitterApi::Tasks::CheckAndUpdateTweetExistence
       not_alive_tweet_records.each do |record|
         record.destroy
       end
+
+      sleep(10)
     end
   end
 
-  def self.debug_each_tweet_execute(all_tweet_numbers)
-    # all_tweet_numbers = [1010167033432948737]
+  def self.execute_with_argument(all_tweet_numbers)
     divided_tweet_numbers = all_tweet_numbers.each_slice(100).to_a
 
     divided_tweet_numbers.each do |tweet_numbers|
@@ -55,9 +36,7 @@ class TwitterApi::Tasks::CheckAndUpdateTweetExistence
         record.destroy
       end
 
-      p alive_tweet_numbers
-      p not_alive_tweet_numbers
-      p not_alive_tweet_records
+      sleep(10)
     end
   end
 end
