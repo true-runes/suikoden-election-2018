02bf9a3ffdf6b503d2657a689c3bc04359547b50
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-24 00:05:26 +0900

    [fix] fix wday method reciever

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def time_type_to_japanese_datetime_str(time_type_object)
-    time_type_object.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[Time.now.wday]}) %H:%M:%S")
+    time_type_object.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[time_type_object.wday]}) %H:%M:%S")
   end
 
   def normalize_tweet_text(tweet_text)
