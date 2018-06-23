3d141f5eb9d2d5422e6d1b6545f674135f0bd2af
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 15:50:12 +0900

    [update] re-update development deploy config file

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -5,7 +5,6 @@ set :branch, :development
 set :deploy_to, ENV["DEPLOY_DIR_FOR_DEV"]
 set :rails_env, 'development'
 server ENV["DEPLOY_SERVER"], user: ENV["DEPLOY_USER_FOR_DEV"], roles: %w{web app db} # wheneverは無効化
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets bundle)
 
 set :ssh_options, {
   port: ENV["DEPLOY_SERVER_PORT"],
@@ -20,6 +19,3 @@ set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.development.pid" # /tmp/pids 
 set :unicorn_config_path, "#{release_path}/config/unicorn.development.rb"
 
 set :pty, false # Sidekiq
-
-# set :bundle_without, 'production' # これがないと BETTOR ERRORS が require されないようだ？
-# https://stackoverflow.com/questions/21841044/capistrano-not-using-rails-environment-with-bundler-properly
