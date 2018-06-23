b3391daede543644f2a4da75a6a145fcbbda4b67
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 11:24:38 +0900

    [update] update yarn directory at local more^7

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,11 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ndenv_type, :user
 set :ndenv_node, '10.5.0'
-set :yarn_target_path, -> { release_path.join('bin/yarn') } #=> なぜ /user/bin/env になるかというとそこへのエイリアス？
+# set :yarn_target_path, -> { release_path.join('bin') } #=> なぜ /user/bin/env になるのだ……
+# 00:05 yarn:install
+#       01 yarn install --production
+#       01 /usr/bin/env: `yarn': そのようなファイルやディレクトリ⿮..
+set :yarn_target_path, -> { current_path.join('bin') } #=> なぜ /user/bin/env になるのだ……
 
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
