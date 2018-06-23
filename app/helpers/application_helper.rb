98b7df2164a23650db999a39adf99362475088b6
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 20:36:58 +0900

    [update] update shown text (view) a little

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def time_type_to_japanese_datetime_str(time_type_object)
+    time_type_object.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[Time.now.wday]}) %H:%M:%S")
+  end
 end
