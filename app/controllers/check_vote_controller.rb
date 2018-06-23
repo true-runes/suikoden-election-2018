62879811c689b2724486d7c962ed2724daca47a0
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 15:27:57 +0900

    [add] add check vote page prototype

--- app/controllers/check_vote_controller.rb
+++ app/controllers/check_vote_controller.rb
@@ -1,7 +1,24 @@
 class CheckVoteController < ApplicationController
- def index
- end
+  def index
+    if params[:screen_name]
+      @screen_name = params[:screen_name]
 
- def create
- end
+      if User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0, tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo') }).first.nil?
+        @target_tweets = []
+      else
+        @target_tweets = User.joins(:tweets).includes(:tweets).where(screen_name: @screen_name, tweets: { is_retweet: 0, tweeted_at: '2018-06-22 21:00:00'.in_time_zone('Tokyo')..'2018-06-24 09:00:00'.in_time_zone('Tokyo') }).first.tweets
+      end
+
+      @vote_number = @target_tweets.size
+
+      if User.where(screen_name: @screen_name).first.nil?
+        @user_name = nil
+      else
+        @user_name = User.where(screen_name: @screen_name).first.name
+      end
+    end
+  end
+
+  def create
+  end
 end
