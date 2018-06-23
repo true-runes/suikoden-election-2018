b6f2be5c0af216d219f66a14fd74178d842e39c2
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 15:29:25 +0900

    [update] update get tweets schedule for development (truely, should clone db itself)

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,12 @@ set :environment, rails_env
 set :output, "#{Rails.root}/log/whenever.log"
 env :PATH, ENV['PATH']
 
+if @environment.to_sym == :development
+  every 5.minute, roles: [:batch] do
+    runner "TwitterApi::Tasks::SearchAndUpsertTweets.execute(search_word: '#幻水総選挙2018')"
+  end
+end
+
 if @environment.to_sym == :production
   every 1.minute, roles: [:batch] do
     runner "TwitterApi::Tasks::SearchAndUpsertTweets.execute(search_word: '#幻水総選挙2018')"
