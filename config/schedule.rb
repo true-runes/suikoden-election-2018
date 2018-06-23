e7b7fda1a2696223873b50237ecc4d3c076c0258
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-06-23 14:09:22 +0900

    [modify] fix whenever scheduling deployment

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,8 @@ set :environment, rails_env
 set :output, "#{Rails.root}/log/whenever.log"
 env :PATH, ENV['PATH']
 
-every 1.minute do
-  runner "TwitterApi::Tasks::SearchAndUpsertTweets.execute(search_word: '#幻水総選挙2018')"
+if @environment.to_sym == :production
+  every 1.minute, roles: [:batch] do
+    runner "TwitterApi::Tasks::SearchAndUpsertTweets.execute(search_word: '#幻水総選挙2018')"
+  end
 end
